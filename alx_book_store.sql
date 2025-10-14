73fae308d06d34d3b42a322db572d16f6e0a7020
Cecilia-Grace <cecigrace2003@gmail.com>
2025-10-14 19:32:34 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS "Customers" (
 	"customer_id "	INTEGER,
 	"customer_name"	VARCHAR(215), 
 	"email"	VARCHAR(215),
-	"address"   TEXT,
+	"address" TEXT,
 	PRIMARY KEY("customer_id ")
 );
 DROP TABLE IF EXISTS "Orders";
