24173045c25260b22a927a3c5e37afa139e6d360
Cecilia-Grace <cecigrace2003@gmail.com>
2025-10-14 19:45:24 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS "Customers" (
 );
 DROP TABLE IF EXISTS "Orders";
 CREATE TABLE IF NOT EXISTS "Orders" (
-	order_id 	INTEGER,
+	order_id INTEGER,
 	customer_id INTEGER,
 	order_date	DATE,
 	FOREIGN KEY("customer_id ") REFERENCES "Customers"("customer_id "),
